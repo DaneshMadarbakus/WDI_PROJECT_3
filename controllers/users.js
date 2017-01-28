da2f4c4f186961ad3f55ebb64d1139016c55b351
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-28 18:32:25 +0000

    added empty controller functions to build on

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,11 @@ const User = require('../models/user.js');
 
 function usersIndex(req, res){
   User.Find((err, users) => {
-    if (err) return res.status(500).json({message:'Something went wrong.'});
+    if (err) return res.status(500).json({message: 'Something went wrong.'});
   });
   return res.status(200).json(users);
 }
+
+module.exports = {
+  index: usersIndex
+};
