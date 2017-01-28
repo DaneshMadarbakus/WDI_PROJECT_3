5503f7e6a29656ec27a8263f33be6cb0a1ec12c0
rainede <rainey@techgiver.com>
2017-01-28 13:54:57 +0000

    Getting express to run before implementing models etc

---
+++ index.js
@@ -0,0 +1,9 @@
+const express     = require('express');
+const morgan      = require('morgan');
+const bodyParser  = require('body-parser');
+const expressJWT  = require('express-jwt');
+const mongoose    = require('mongoose');
+const app         = express();
+
+app.use(morgan('dev'));
+app.listen(3000, ()=> console.log('Express has started'));
