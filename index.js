ce278e3a2d64f75f2686bd3ef09e661be741efb4
DaneshMadarbakus <daneshplt@hotmail.co.uk>
2017-01-28 17:42:50 +0000

    added static controller and route and updated index.js

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ const morgan      = require('morgan');
 const bodyParser  = require('body-parser');
 const expressJWT  = require('express-jwt');
 const mongoose    = require('mongoose');
+const cors        = require('cors');
 const app         = express();
 
+const routes      = require('config/routes');
+const config      = require('config/config');
 
-app.get('/*', (req, res) => res.sendFile(`src/index.html`));
+mongoose.connect(config.db);
 
 app.use(morgan('dev'));
-app.listen(3000, ()=> console.log('Express has started'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(`${__dirname}/public`));
+app.use('/api', expressJWT({ secret: config.secret })
+  .unless({
+    path: [
+      { url: '/api/register', methods: ['POST']},
+      { url: '/api/login', methods: ['POST']}
+    ]
+  }));
+app.use(jwtErrorHandler);
+
+function jwtErrorHandler(err, req, res, next){
+  if (err.name !== 'UnauthorizedError') return next();
+  return res.status(401).json({ message: 'UnauthorizedRequest.' });
+}
+
+app.use('/', routes);
+
+app.listen(config.port, console.log(`app is listening on ${config.port}`));
