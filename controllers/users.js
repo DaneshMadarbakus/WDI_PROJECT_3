c8fb52a547d765f62a5c98247091fe1d191f4849
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-28 18:37:46 +0000

    fixed user index function

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,21 @@
-const User = require('../models/user.js');
+const User = require('../models/user');
 
 function usersIndex(req, res){
-  User.Find((err, users) => {
+  User.find((err, users) => {
     if (err) return res.status(500).json({message: 'Something went wrong.'});
+    return res.status(200).json(users);
   });
-  return res.status(200).json(users);
+}
+
+function usersShow(req, res){
+}
+
+function usersUpdate(req, res){
+
+}
+
+function usersDelete(req, res){
+
 }
 
 module.exports = {
