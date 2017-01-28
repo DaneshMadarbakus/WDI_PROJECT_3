168eeec7fab554fdbf7f1dd0e8b2b005065e4591
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-28 20:09:09 +0000

    added routes for users in route.js

--- config/routes.js
+++ config/routes.js
@@ -7,6 +7,11 @@ const usersController = require('../controllers/users');
 router.route('/')
   .get(staticsController.home);
 
-
+router.route('/users')
+  .get(usersController.index);
+router.route('/users/:id')
+  .get(usersController.show)
+  .put(usersController.update)
+  .delete(usersController.delete);
 
 module.exports = router;
