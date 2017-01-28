abdbaa95baf1d97e872e0fc2fae5ee991817cf14
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-28 18:54:11 +0000

    added usersUpdate to users controller

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,11 @@ function usersShow(req, res){
 }
 
 function usersUpdate(req, res){
-
+  User.findByIdAndUpdate(req.params.id, req.body.user, { new: true}, (err, user) => {
+    if (err) return res.status(500).json({message: 'Something went wrong trying update User'});
+    if(!user) return res.status(404).json({message: 'No User was found '});
+    return res.status(200).json(user);
+  });
 }
 
 function usersDelete(req, res){
@@ -25,5 +29,6 @@ function usersDelete(req, res){
 
 module.exports = {
   index: usersIndex,
-  show: usersShow
+  show: usersShow,
+  update: usersUpdate
 };
