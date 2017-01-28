fc3c013a374aee6afca812d9aa21610ef97ab06b
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-28 21:08:28 +0000

    added userAuthentication function

--- controllers/user-authentication-controller.js
+++ controllers/user-authentication-controller.js
@@ -1,3 +1,18 @@
 const User   = require('../models/user');
 const jwt    = require('jsonwebtoken');
 const config = require('../config/config');
+
+function userAuthenticationsRegister(req, res){
+  User.create(req.body, (err, user) => {
+    if (err) return res.status(500).json({ message: 'Something went wrong with Authenticating a new User'});
+    const token = jwt.sign({id: user.id}, config.secret, {expiresIn: 60*60*24*7});
+    return res.status(201).json({
+      user,
+      token
+    });
+  });
+}
+
+module.exports = {
+  userRegister: userAuthenticationsRegister(),
+};
