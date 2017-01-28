b502ec895c0a3f437c16cbedb94afea92bc63026
ebere10 <ebere10@hotmail.com>
2017-01-28 17:11:39 +0000

    started adding functions for employer controller

---
+++ controllers/employers.js
@@ -0,0 +1,23 @@
+
+
+const Employer = require('../models/employer');
+
+
+function employersIndex(){
+  
+}
+
+function employersUpdate(){
+
+}
+
+function employersDelete(){
+
+}
+
+
+module.exports = {
+  index: employersIndex,
+  update: employersUpdate,
+  delete: employersDelete
+};
