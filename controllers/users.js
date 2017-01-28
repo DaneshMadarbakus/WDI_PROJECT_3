2020d8e7b0c3523fe441f4e7d2dd98da45a2d4b5
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-28 18:48:33 +0000

    added to usersShow function to allow it to return json when there is a successful request

--- controllers/users.js
+++ controllers/users.js
@@ -11,6 +11,7 @@ function usersShow(req, res){
   User.findById(req.params.id, (err, user) =>{
     if (err) return res.status(500).json({message: 'Something went wrong trying show User'});
     if(!user) return res.status(404).json({message: 'No User was found '});
+    return res.status(200).json(user);
   });
 }
 
