8990bfdbb23561ab3a8ca8018ffe29e4298f0636
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-29 02:24:21 +0000

    added userAuthentication function

--- controllers/user-authentication-controller.js
+++ controllers/user-authentication-controller.js
@@ -4,7 +4,21 @@ const config = require('../config/config');
 
 function userAuthenticationsRegister(req, res){
   User.create(req.body, (err, user) => {
-    if (err) return res.status(500).json({ message: 'Something went wrong with Authenticating a new User'});
+    if (err) return res.status(500).json({ message: 'Something went wrong with authenticating a new user'});
+    const token = jwt.sign({id: user.id}, config.secret, {expiresIn: 60*60*24*7});
+    return res.status(201).json({
+      user,
+      token
+    });
+  });
+}
+
+function userAuthenticationLogin(req, res){
+  User.findOne({ email: req.body.email}, (err, user) => {
+    if(err) return res.status(500).json({message: 'something went wrong with  authenticating user login'});
+    if(!user || !user.validatePassword(req.body.password)){
+      return res.status(401).json({message: 'Unauthorized'});
+    }
     const token = jwt.sign({id: user.id}, config.secret, {expiresIn: 60*60*24*7});
     return res.status(201).json({
       user,
@@ -14,5 +28,6 @@ function userAuthenticationsRegister(req, res){
 }
 
 module.exports = {
-  userRegister: userAuthenticationsRegister(),
+  userRegister: userAuthenticationsRegister,
+  userLogin: userAuthenticationLogin
 };
