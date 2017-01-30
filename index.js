716f9798515bc17124a372d676223f05bd5db8b2
rainede <rainey@techgiver.com>
2017-01-30 18:15:25 +0000

    comapny show page fetching data correctly

--- index.js
+++ index.js
@@ -25,19 +25,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(`${__dirname}/public`));
-app.use('/api', expressJWT({ secret: config.secret })
-  .unless({
-    path: [
-      { url: '/api/register', methods: ['POST']},
-      { url: '/api/login', methods: ['POST']}
-    ]
-  }));
-app.use(jwtErrorHandler);
-
-function jwtErrorHandler(err, req, res, next){
-  if (err.name !== 'UnauthorizedError') return next();
-  return res.status(401).json({ message: 'UnauthorizedRequest.' });
-}
+// app.use('/api', expressJWT({ secret: config.secret })
+//   .unless({
+//     path: [
+//       { url: '/api/register', methods: ['POST']},
+//       { url: '/api/login', methods: ['POST']}
+//     ]
+//   }));
+// app.use(jwtErrorHandler);
+//
+// function jwtErrorHandler(err, req, res, next){
+//   if (err.name !== 'UnauthorizedError') return next();
+//   return res.status(401).json({ message: 'UnauthorizedRequest.' });
+// }
 
 app.use('/api', routes);
 app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
