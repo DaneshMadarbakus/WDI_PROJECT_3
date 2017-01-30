97957cfc506d68c236e6e1945bc4d3427d330f59
rainede <rainey@techgiver.com>
2017-01-30 10:45:51 +0000

    added 'new' to companies controller

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,5 +1,9 @@
 const User    = require('../models/user');
-const Companies = require('../models/company');
+const Company = require('../models/company');
+
+function companiesNew(req, res){
+  return res.render('companies/new', {error: null});
+}
 
 function companiesCreate(req, res){
   User.findById(req.params.id, (err, user) => {
