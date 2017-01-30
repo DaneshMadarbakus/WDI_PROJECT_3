b7af6d20432848c5cb1a79115393caee88e72c91
ebere10 <ebere10@hotmail.com>
2017-01-30 13:09:54 +0000

    added home state to router.config

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -2,7 +2,7 @@ angular
   .module('clementine')
   .config(Router);
 
-  Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
-  function Router($stateProvider, $locationProvider, $urlRouterProvider){
-    
-  }
+Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+function Router($stateProvider, $locationProvider, $urlRouterProvider){
+  $stateProvider.state('home', {url: '/', templateUrl: '/js/views/home.html'});
+}
