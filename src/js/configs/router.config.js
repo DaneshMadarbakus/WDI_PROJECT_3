efbd74aea29483ca889b4c6f007140afd7d30aba
ebere10 <ebere10@hotmail.com>
2017-01-30 13:06:00 +0000

    setting up router.config.js

---
+++ src/js/configs/router.config.js
@@ -0,0 +1,8 @@
+angular
+  .module('clementine')
+  .config(Router);
+
+  Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    
+  }
