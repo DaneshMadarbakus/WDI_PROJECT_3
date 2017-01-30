d6249f82c2e17a52f7009a199f609f20c8037976
ebere10 <ebere10@hotmail.com>
2017-01-30 14:39:38 +0000

    deleted unnecessary braces

--- src/js/controllers/login.controller.js
+++ src/js/controllers/login.controller.js
@@ -6,14 +6,12 @@ LoginCtrl.$inject = ['User'];
 function LoginCtrl(User) {
   const vm = this;
 
-  vm.login = () => console.log('I work! Sort of.')
-    User.login(vm.user)
-    .$promise
-    .then(data => {
-      console.log(data);
-    }, err => {
-      console.log(err);
-    });
-  };
-
+  vm.login = () => console.log('I work! Sort of.');
+  User.login(vm.user)
+  .$promise
+  .then(data => {
+    console.log(data);
+  }, err => {
+    console.log(err);
+  });
 }
