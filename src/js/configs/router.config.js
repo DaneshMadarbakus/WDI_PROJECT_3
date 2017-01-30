aefc33a82f549a07e4177834fc7a6b2bc54901d4
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-30 13:48:46 +0000

    added location provider to router function in router.config.js

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -4,6 +4,7 @@ angular
 
 Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
 function Router($stateProvider, $locationProvider, $urlRouterProvider){
+  $locationProvider.html5Mode(true);
   $stateProvider
   .state('home', {
     url: '/', templateUrl: '/js/views/home.html'
