9585a10515e8b152d75408a7fe90d744499965b2
hachtman <jackfllr1@googlemail.com>
2017-01-30 18:28:00 +0000

    fixed second %7d bastard issue

--- src/js/factories/user.factory.js
+++ src/js/factories/user.factory.js
@@ -7,7 +7,7 @@ userFactory.$inject = ['API', '$resource'];
 function userFactory(API, $resource) {
 
   return $resource(`${API}/users/:id`, {id: '@_id'},
-    {'register': {method: 'POST', url: `${API}/register}`},
+    {'register': {method: 'POST', url: `${API}/register`},
       'login': { method: 'POST', url: `${API}/login` }
     });
 }
