1b75ca90e1a458f09ae279724722abf90189b03e
ebere10 <ebere10@hotmail.com>
2017-01-30 12:01:41 +0000

    set-up login controller

--- src/js/controllers/login.controller.js
+++ src/js/controllers/login.controller.js
@@ -1,8 +1,19 @@
 angular
-  .module('clementine')
-  .controller('loginCtrl', loginCtrl);
+.module('clementine')
+.controller('loginCtrl', loginCtrl);
 
-  loginCtrl.$inject = ['User'];
-  function loginCtrl(User) {
-    
-  }
+loginCtrl.$inject = ['User'];
+function loginCtrl(User) {
+  const vm = this;
+
+  vm.login = () => {
+    User.login(vm.user)
+    .$promise
+    .then(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
+  };
+
+}
