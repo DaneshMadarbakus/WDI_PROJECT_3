dfa022c3d7bdb57350d64f6f8ba3dfabc8716063
hachtman <jackfllr1@googlemail.com>
2017-01-30 11:09:31 +0000

    installed chai and supertest, set up spec helper

---
+++ test/spec_helper.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = 'test';
+
+//require th chai testing library
+const chai = require('chai');
+//Assigns 'should' and  'expect' to the chai should and expect methods
+global.should = chai.should();
+global.expect = chai.expect();
+
+//creates the app for supertest
+const supertest = require('supertest');
+const app       = require('../index');
+global.api      = supertest(app);
