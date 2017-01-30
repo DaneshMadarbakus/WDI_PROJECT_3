6ce1663ce2b18266c126cf0a7aa74b9defc5d844
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-01-30 19:36:59 +0000

    added decode functions both

--- src/js/controllers/login.controller.js
+++ src/js/controllers/login.controller.js
@@ -2,16 +2,18 @@ angular
 .module('clementine')
 .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['User'];
-function LoginCtrl(User) {
+LoginCtrl.$inject = ['User', 'CurrentUserService'];
+function LoginCtrl(User, CurrentUserService) {
   const vm = this;
-  
+
   vm.login = () => {
-    console.log('I work! Sort of.');
-    User.login(vm.user)
-    .$promise
-    .then(data => {
-      console.log(data);
-    });
+    User
+      .login(vm.user)
+      .$promise
+      .then(() => {
+        CurrentUserService.getUser();
+      }, err => {
+        console.log(err);
+      });
   };
 }
--- src/js/controllers/register.controller.js
+++ src/js/controllers/register.controller.js
@@ -2,16 +2,17 @@ angular
 .module('clementine')
 .controller('RegisterCtrl', RegisterCtrl);
 
-RegisterCtrl.$inject = ['User'];
+RegisterCtrl.$inject = ['User', 'CurrentUserService'];
 function RegisterCtrl(User){
   const vm = this;
-  vm.register = function() {
+  vm.register = function(CurrentUserService) {
     console.log(vm.user);
-    User.register(vm.user)
-    .$promise
-    .then(data => {
-      console.log(data);
-  
+    User
+    .register(vm.user).$promise
+    .then(() => {
+      CurrentUserService.getUser();
+    }, err => {
+      console.log(err);
     });
   };
 }
