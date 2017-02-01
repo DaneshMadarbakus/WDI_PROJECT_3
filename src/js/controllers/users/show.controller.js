e50873528e004176871c672f9a4aa24300a2d111
hachtman <jackfllr1@googlemail.com>
2017-02-01 09:59:18 +0000

    replaced looped http request with .populate

--- src/js/controllers/users/show.controller.js
+++ src/js/controllers/users/show.controller.js
@@ -6,33 +6,21 @@ usersShowCtrl.$inject = ['User','Company', '$stateParams', '$http', 'API'];
 function usersShowCtrl(User, Company, $stateParams, $http, API) {
   const vm = this;
 
-  User.get($stateParams, function(data) {
-    vm.user = data;
-    console.log(vm.user);
-    // extract the companies array, init returned array
-    const c = data.companies;
-    const returned = [];
-    //loop over the ids, request relevant information and pass into returned array.
-    for (var i = 0; i < c.length; i++) {
-      $http({
-        method: 'GET',
-        url: `${API}/companies/${data.companies[i]}`
-      }).then((data) => {
-        console.log(data.data.createdAt);
-        data.data.createdAt = createdOnParser(data.data.createdAt);
-        returned.push(data.data);
-      });
-      console.log(returned);
-    }
-    //assign the returned array to the function to access it in the view.
-    vm.items = returned;
+  //fetch the user and companies. 
+  User
+  .get($stateParams)
+  .$promise
+  .then(response => {
+    vm.user = response;
+    vm.items = response.companies;
+    vm.items.forEach(item => {
+      item.createdAt = createdOnParser(item.createdAt);
+    });
   });
 }
 
 //make sense of the timestamps.
 function createdOnParser(data) {
-  console.log('ping');
-  console.log(data, 'before');
   var str  = data.split('T');
   var date = str[0];
   var time = str[1].split('.')[0];
