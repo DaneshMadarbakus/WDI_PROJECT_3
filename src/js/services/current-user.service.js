acf4369948722bc511613c3ec8b60fe000e947c3
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-01 05:00:51 +0000

    made randName Service and applied it current user service, now we need to make changes to user and the jwt token.

--- src/js/services/current-user.service.js
+++ src/js/services/current-user.service.js
@@ -2,14 +2,15 @@ angular
   .module('clementine')
   .service('CurrentUserService', CurrentUserService);
 
-CurrentUserService.$inject = ['TokenService', '$rootScope'];
-function CurrentUserService(TokenService, $rootScope){
+CurrentUserService.$inject = ['TokenService', '$rootScope', 'randNameService'];
+function CurrentUserService(TokenService, $rootScope, randNameService){
   let currentUser = TokenService.decodeToken();
-
+  const randomUsername = randNameService.rndName();
   return {
     user: currentUser,
     saveUser(user){
       user.id = user.id ? user.id : user._id;
+      // user.username = randomUsername;
       currentUser = user;
       $rootScope.$broadcast('loggedIn');
     },
